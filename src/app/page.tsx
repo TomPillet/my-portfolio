e997e01e3c5fea5b1637cb93317495b1e2e87af3
TomPillet <tom.pilletgaulon@gmail.com>
2025-05-01 17:48:13 +0200

    feat: add content layout on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
-import { Button, Box, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
+"use client";
+import SplitText from "@/reactbits/text-animations/SplitText/SplitText";
+import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Flex>
-      Accueil
-        {/* <Grid templateColumns="repeat(2, 1fr)" border="2px solid #c3c3c3" rounded="32px" overflow={"hidden"}>
-          <GridItem h="full" bg="#131313">
-          </GridItem>
-          <GridItem h="full" bg="#101010" px="16" py="12">
-            <Flex w="full" justifyContent="space-around">
-              <Link href="/competences">
-                <Button link cursor={"pointer"}>Compétences</Button>
-              </Link>
-              <Link href="/projets">
-                <Button link cursor={"pointer"}>Projets</Button>
-              </Link>
-              <Link href="/contact">
-                <Button link cursor={"pointer"}>Contact</Button>
-              </Link>
-            </Flex>
-            <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultricies elit ut ultrices suscipit. Aliquam sodales tristique ipsum. Vestibulum eu lectus lacinia justo pretium volutpat.</Text>
-          </GridItem>
-        </Grid> */}
+    <Flex fontSize={"5rem"} letterSpacing={"-2px"} w="full" justifyContent="center" alignItems="center" flexDir={"column"}>
+      <SplitText
+        text="Tom Pillet-Gaulon"
+        className="text-2xl font-semibold text-center"
+        delay={80}
+        animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+        animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+        threshold={0.2}
+        easing={(t) => t}
+        rootMargin="-50px"
+        onLetterAnimationComplete={() => {console.log('complete')}}
+      />
+      <Grid h={"400px"} w={"1/2"} 
+      templateRows="repeat(2, 1fr)" templateColumns={"repeat(2, 1fr)"}>
+        <GridItem rowSpan={2} bg={"red"}></GridItem>
+        <GridItem bg={"blue"}></GridItem>
+        <GridItem bg={"green"}></GridItem>
+      </Grid>
     </Flex>
   );
 }
