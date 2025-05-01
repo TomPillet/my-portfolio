946e7c2cd3fdb2bd3f98a3406b566c2919754a22
TomPillet <tom.pilletgaulon@gmail.com>
2025-05-01 15:46:59 +0200

    refactor: move buttonRecipe in separated file

---
+++ my-portfolio/src/chakra/button.ts
@@ -0,0 +1,56 @@
+import { defineRecipe } from "@chakra-ui/react"
+
+const pseudoNavStyles = {
+    content: '""',
+    position: "absolute",
+    left: "0",
+    width: "0%",
+    height: "100%",
+    transition: "all 0.3s ease-in-out",
+    // backgroundImage: "linear-gradient(to bottom, rgba(255,255,255,.01), transparent)",
+}
+
+export const buttonRecipe = defineRecipe({
+    variants: {
+        link: {
+            true: {
+                position: "relative",
+                // background: "linear-gradient(to bottom, rgba(255,255,255,.05), transparent)",
+                background: "transparent",
+                color: "rgb(229, 229, 229)",
+                border: "none",
+                height: "100%",
+                borderRadius: "0px",
+                fontSize: "24px",
+                fontWeight: "400",
+                letterSpacing: "1px",
+                textTransform: "uppercase",
+                transition: "all 0.3s ease-in-out",
+
+                // _after: {
+                //     ...pseudoNavStyles,
+                //     backdropFilter: "invert(1)",
+                // },
+
+                _before: {
+                    ...pseudoNavStyles,
+                    // background: "black",
+                    borderTop: "2px solid white",
+                    zIndex: "-1",
+                },
+
+                _hover: {
+                    // background: "linear-gradient(to bottom, rgba(255,255,255,.05), transparent)",
+                    background: "transparent",
+                    letterSpacing: "8px",
+                    // _after: {
+                    //     width: "100%",
+                    // },
+                    _before: {
+                        width: "100%",
+                    }
+                }
+            },
+        },
+    }  
+})
--- my-portfolio/src/chakra/theme.ts
+++ my-portfolio/src/chakra/theme.ts
@@ -1,54 +1,5 @@
-import { createSystem, defaultConfig, defineConfig, defineRecipe } from "@chakra-ui/react";
-
-const pseudoNavStyles = {
-    content: '""',
-    position: "absolute",
-    left: "0",
-    width: "0%",
-    height: "100%",
-    transition: "all 0.3s ease-in-out",
-}
-
-const buttonRecipe = defineRecipe({
-    variants: {
-        link: {
-            true: {
-                position: "relative",
-                background: "transparent",
-                color: "rgb(229, 229, 229)",
-                height: "100%",
-                borderRadius: "0px",
-                fontSize: "24px",
-                fontWeight: "400",
-                letterSpacing: "1px",
-                textTransform: "uppercase",
-                transition: "all 0.3s ease-in-out",
-
-                _after: {
-                    ...pseudoNavStyles,
-                    backdropFilter: "invert(1)",
-                },
-
-                _before: {
-                    ...pseudoNavStyles,
-                    background: "black",
-                    zIndex: "-1",
-                },
-
-                _hover: {
-                    background: "transparent",
-                    letterSpacing: "8px",
-                    _after: {
-                        width: "100%",
-                    },
-                    _before: {
-                        width: "100%",
-                    }
-                }
-            },
-        },
-    }  
-})
+import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
+import { buttonRecipe } from "./button";
 
 const customConfig = defineConfig({ 
     theme: {
