b60f294787633ea3f15aeea79ab667fdc51b8048
Tom <tom@cadcom.fr>
2025-05-20 13:24:08 +0200

    refactor: rename skillLevel service file to skillLevelService

--- src/app/services/skillLevelService.ts
+++ src/app/services/skillLevelService.ts
@@ -1,7 +1,6 @@
 export async function getSkillLevelById(id: number) {
   try {
     const response = await fetch(`/api/skill-level/${id}`, { method: "GET" });
-    console.log(response);
     return await response.json();
   } catch (error) {
     console.error("Error fetching users:", error);
