e0daed12b9018932c80024b4efac516c2e3cd551
Tom <tom@cadcom.fr>
2025-05-20 14:22:07 +0200

    refactor: use ts instead of js for chakra/config files

---
+++ src/chakra/config/theme.ts
@@ -0,0 +1,12 @@
+import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
+import { buttonRecipe } from "./button";
+
+const customConfig = defineConfig({
+  theme: {
+    recipes: {
+      button: buttonRecipe,
+    },
+  },
+});
+
+export const system = createSystem(defaultConfig, customConfig);
