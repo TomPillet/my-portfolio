670d0f2070db66366683a5f9428b044017e8fa3c
TomPillet <tom963741@gmail.com>
2025-05-24 12:06:05 +0200

    feat: add recaptcha at mailing form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          src="https://www.google.com/recaptcha/api.js"
+          async
+          defer
+        ></script>
+      </head>
       <body className={`${ubuntu.className} ${dmSans.className} antialiased`}>
         <Provider>
           <Header />
